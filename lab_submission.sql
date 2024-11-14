235fe022a8fb7b98a4d9dee10c4c1dee03e3ea32
AnanJUMA <167190510+AnanJUMA@users.noreply.github.com>
2024-11-14 15:55:44 +0300

    Update lab_submission.sql
    Signed-off-by: AnanJUMA <167190510+AnanJUMA@users.noreply.github.com>

--- lab_submission.sql
+++ lab_submission.sql
@@ -1,1 +1,17 @@
+
 -- CREATE EVN_average_customer_waiting_time_every_1_hour
+SET GLOBAL event_scheduler = ON;
+CREATE EVENT EVN_average_customer_waiting_time_every_1_hour
+ON SCHEDULE EVERY 1 HOUR
+DO
+BEGIN
+INSERT INTO customer_service_kpi (customer_service_KPI_average_waiting_time_minutes, customer_service_KPI_timestamp)
+    SELECT 
+        AVG(TIMESTAMPDIFF(MINUTE, ticket_created_time, ticket_resolved_time)), 
+        NOW()
+    FROM 
+        customer_sevice_ticket
+    WHERE 
+         ticket_created_time >= NOW() - INTERVAL 1 HOUR
+        AND ticket_resolved_time IS NOT NULL;
+END
